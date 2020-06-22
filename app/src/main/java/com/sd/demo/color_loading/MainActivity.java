4360d3166aeb39f43126dbc11dcb03614e4b5bf8
zjun <565061763@qq.com>
2020-06-22 10:18:44 +0800

    demo

--- app/src/main/java/com/sd/demo/color_loading/MainActivity.java
+++ app/src/main/java/com/sd/demo/color_loading/MainActivity.java
@@ -4,26 +4,26 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.sd.demo.color_loading.databinding.ActivityMainBinding;
+import com.sd.lib.cloading.view.FLinearColorLoadingView;
 
 public class MainActivity extends AppCompatActivity
 {
-    private ActivityMainBinding mBinding;
+    private FLinearColorLoadingView view_loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(mBinding.getRoot());
+        setContentView(R.layout.activity_main);
+        view_loading = findViewById(R.id.view_loading);
 
-        mBinding.viewLoading.start();
+        view_loading.start();
     }
 
     @Override
     protected void onDestroy()
     {
         super.onDestroy();
-        mBinding.viewLoading.stop();
+        view_loading.stop();
     }
 }
